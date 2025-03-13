bb3ecbe2073986e666d05c49cfd9adaa87935443
PnSts <pns_tsoukalas@hotmail.com>
2025-03-13 22:20:28 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 // import 'modern-normalize/modern-normalize.css';
 import './index.css';
 // import ScrollToTop from "./components/layout/ScrollToTop";
@@ -10,10 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* <ScrollToTop /> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
